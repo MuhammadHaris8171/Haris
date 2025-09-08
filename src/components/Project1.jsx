f3b8dbba52c6b52c3d6e7b34ee59b72fb49350be
MuhammadHaris8171 <yassuopro316@gmail.com>
2025-09-08 09:05:42 +0500

    asd

--- src/components/Project1.jsx
+++ src/components/Project1.jsx
@@ -103,6 +103,7 @@ export default function Project1() {
     <div className={`bg-white min-h-screen p-8 overflow-hidden projectMain`}>
       <div className="max-w-7xl mx-auto">
         <h1 className="text-4xl font-bold text-gray-900 mb-8">My Projects</h1>
+        <p>Following are the Projects that either i collaborated or completely developed myself</p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {projects.map((project, index) => (
             <div
